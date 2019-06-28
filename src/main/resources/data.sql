70cb4f025668bd5916242be7a4701f0976e247de
jsapp352 <32817628+jsapp352@users.noreply.github.com>
2019-06-28 09:33:45 -0400

    Added data.sql to initialize records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO instructor (id, name, pin) VALUES
+  ('1', 'DJ Paul', '36'),
+  ('2', 'Juancho', '11'),
+  ('3', 'Cindy', '5595'),
+  ('4', 'MonsterMash', '445'),
+  ('5', 'Zoomacroom', '101');
